f1c53cd1401483773cb707d9d1aa0f2896caebb5
anderson <anderson.hamamoto@gmail.com>
2013-08-16 10:11:26 -0300

    criado uma struct em address_book.c para armazenar cada entrada na agenda

--- address_book.c
+++ address_book.c
@@ -2,18 +2,16 @@
 #include <stdlib.h>
 #include <mysql.h>
 
+typedef struct entry entry;
+
+struct entry {
+	char *name;
+	char *addr; 
+	char *phone;
+};
+
 int initialize(MYSQL *conn, FILE *file);
-void insert(MYSQL *conn, char *name, char *phone, char *addr);
-int alter(MYSQL *conn, char *name);
-int remove(MYSQL *conn, char *name);
+void insert(MYSQL *conn, entry *data);
+int alter(MYSQL *conn, entry *data);
+int delete(MYSQL *conn, entry *data);
 void list(MYSQL *conn);
-
-void initialize(MYSQL *conn) {}
-
-void insert(MYSQL *conn, char *name, char *phone, char *addr) {}
-
-int alter(MYSQL *conn, char *name) {}
-
-void remove(MYSQL *conn, char *name) {}
-
-void list(MYSQL *conn) {}
